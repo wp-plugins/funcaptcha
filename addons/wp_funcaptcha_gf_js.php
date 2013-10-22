59a531b04eff57fa153b65774510c2cad0ec4a37
SwipeAds <SwipeAds@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-22 09:54:28 +0000

    0.4.1 update.
    git-svn-id: https://plugins.svn.wordpress.org/funcaptcha/tags/0.4.1@791713 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ addons/wp_funcaptcha_gf_js.php
@@ -0,0 +1,23 @@
+<script type="text/javascript">
+//JS for Gravity Forms.
+if (fieldSettings.FunCaptcha === undefined)
+	fieldSettings["FunCaptcha"] = ".label_setting, .css_class_setting";
+
+function funcaptcha_gf_add_field(name) {
+    var nextId = GetNextFieldId();
+    var field = new Field(nextId, name);	
+	field.label = "<?php _e('Verification', 'gravityforms'); ?>";
+
+	var mysack = new sack("<?php echo admin_url('admin-ajax.php')?>?id=" + form.id);
+    mysack.execute = 1;
+    mysack.method = 'POST';
+    mysack.setVar( "action", "rg_add_field" );
+    mysack.setVar( "rg_add_field", "<?php echo wp_create_nonce('rg_add_field') ?>" );
+    mysack.setVar( "field", jQuery.toJSON(field) );
+    mysack.encVar( "cookie", document.cookie, false );
+    mysack.onError = function() { alert('<?php echo esc_js(__("Ajax error while adding field", "gravityforms")) ?>' )};
+    mysack.runAJAX();
+
+    return true;
+}
+</script>
