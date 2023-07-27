c7eb621f5f2b73993cd29fef9968d311dd363fcc
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2023-07-27 10:03:53 +1000

    Please delete this repo

--- addons/ajax.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).on("submitResponse.default", function(e, response){
-    jQuery("#fc-ninja").html("<p>Loading new challenge...</p>");
-    jQuery.post(the_ajax_script.ajaxurl,
-        { 'action': 'new_fc' },
-        function(newfc_ajax){
-            jQuery("#fc-ninja").html(newfc_ajax);
-        }
-    );
-});
